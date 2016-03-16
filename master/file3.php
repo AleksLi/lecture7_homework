5344894cacc28aebba4bdb3591febd440a258d2a
Aleks Litvinov <odesey2008@yandex.ua>
2016-03-16 23:23:48 +0200

    MASTER. Fourth commit

---
+++ master/file3.php
@@ -0,0 +1,28 @@
+<?php
+// Создаем новый класс Coor:
+function Getname() {
+    echo $this->name;
+}
+class Coor {
+// данные (свойства):
+    var $name;
+
+// методы:
+    function Getname() {
+        echo $this->name;
+    }
+
+    function Setname($name) {
+        $this->name = $name;
+    }
+
+}
+
+// Создаем объект класса Coor:
+$object = new Coor;
+// Теперь для изменения имени используем метод Setname():
+$object->Setname("Nick");
+// А для доступа, как и прежде, Getname():
+$object->Getname();
+// Сценарий выводит 'Nick'
+?>
