862d9fe4480a9fe9cf67708e54a6ab82b2231196
Aleks Litvinov <odesey2008@yandex.ua>
2016-03-17 00:49:45 +0200

    DEV. Third commit

--- master/file11.php
+++ master/file11.php
@@ -7,8 +7,6 @@
  */
 
 class Par {
-    function parent_funct() { echo "<h1>Это родительская функция</h1>"; }
-    function test () { echo "<h1>Это родительский класс</h1>"; }
 }
 
 class Child extends Parent {
@@ -21,4 +19,8 @@ $object = new Child;
 
 $object->parent_funct(); // Выводит 'Это родительская функция'
 $object->child_funct(); // Выводит 'Это дочерняя функция'
+$object->test(); // Выводит 'Это дочерний класс'
+$object->test(); // Выводит 'Это дочерний класс'
+$object->test(); // Выводит 'Это дочерний класс'
+$object->test(); // Выводит 'Это дочерний класс'
 $object->test(); // Выводит 'Это дочерний класс'
