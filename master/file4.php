14c0e702d6e89d7272ac6b2e52499db387bfc4f7
Aleks Litvinov <odesey2008@yandex.ua>
2016-03-17 00:50:56 +0200

    DEV. Fourth commit

--- master/file4.php
+++ master/file4.php
@@ -3,12 +3,6 @@
 function Getname() {
     echo $this->name;
 }
-class Webpage {
-    var $bgcolor;
-    function Webpage($color) {
-        $this->bgcolor = $color;
-    }
-}
 
 // Вызвать конструктор класса Webpage
 $page = new Webpage("brown");
